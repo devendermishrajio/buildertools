0ca47591987c843b89a1d7c43834bf6c3f978f14
akash1808 <akash.agrawal1992@gmail.com>
2016-01-12 15:05:27 +0530

    Add shell files for backup

---
+++ files/db-backup.sh
@@ -0,0 +1,7 @@
+cd /root/db-backup/AWSS3ConfigManager/configManager
+
+#Create backup
+tmpdir="/tmp/backup-`date +'%Y%d%m'`"
+innobackupex --user={{ db_admin_user }} --password={{ db_admin_password }} $tmpdir
+./upload_db.sh $tmpdir
+
