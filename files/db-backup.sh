928ed94b87f18e7c1f02bc2c3ad8d25f7630b226
akash1808 <akash.agrawal1992@gmail.com>
2016-01-12 15:29:30 +0530

    Add removal of backup dir

--- files/db-backup.sh
+++ files/db-backup.sh
@@ -4,4 +4,5 @@ cd /root/db-backup/AWSS3ConfigManager/configManager
 tmpdir="/tmp/backup-`date +'%Y%d%m'`"
 innobackupex --user={{ db_admin_user }} --password={{ db_admin_password }} $tmpdir
 ./upload_db.sh $tmpdir
+rm -rf $tmpdir
 
